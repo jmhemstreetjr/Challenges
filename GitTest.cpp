faac76a36b8befbd592be565d0d9758dc689f458
jmhemstreetjr <46822910+jmhemstreetjr@users.noreply.github.com>
2019-08-16 10:11:43 -0500

    Add files via upload

---
+++ GitTest.cpp
@@ -0,0 +1,11 @@
+//This is a test file for GitHub
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	std::cout << "Press ENTER to continue...";
+	std::cin.get();
+	return 0;
+}
